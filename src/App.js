36775678873edd9e5202350125270e73e5a67301
Saurav Kumar <maxsaurav267@gmail.com>
2021-03-26 18:31:56 +0530

    imported components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Navbar, FilterBox, ProductCard } from "./components";
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    (async function () {
-      axios.get("/api/products").then((response) => {
-        setData(response.data.products);
-      });
-    })();
-  }, []);
-
+  
   return (
     <div className="App">
-      <h1> Data </h1>
-      <ul>
-        {data.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
+      <Navbar/>
+      <FilterBox/>
+      <ProductCard/>
     </div>
   );
 }
