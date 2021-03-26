3b886133859c77f027e68e44c6c9976e0a490b4b
Saurav Kumar <maxsaurav267@gmail.com>
2021-03-26 18:16:08 +0530

    added context provider

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -1,9 +1,57 @@
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useReducer, useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductList = createContext();
 
 export function useProduct() {
     return useContext(ProductList);
+}
+
+export function ProductProvider({children}){
+    const [ data, setData ] = useState([]);
+
+    useEffect(() => {
+        (async function () {
+            try {
+                const response = await axios.get("/api/products");
+                setData(response.data.products);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+      }, []);
+    
+
+    const [state, dispatch] = useReducer(productReducer,  { showInventoryAll: true, showFastDeliveryOnly: false, sortBy: null });
+
+    return (
+        <ProductList.Provider value={{ data, state, dispatch}}>
+            {children}
+        </ProductList.Provider>
+    )
+}
+
+
+const productReducer = (state, action) => {
+    switch (action.type) {
+        case "TOGGLE_INVENTORY":
+          return (state = {
+            ...state,
+            showInventoryAll: !state.showInventoryAll
+          });
+
+        case "TOGGLE_DELIVERY":
+          return (state = {
+            ...state,
+            showFastDeliveryOnly: !state.showFastDeliveryOnly
+          });
+        case "SORT":
+          return {
+            ...state,
+            sortBy: action.payload
+          };
+        default:
+          return state;
+      }
 }
 
