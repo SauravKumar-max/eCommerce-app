194150444c857d7ebf25d9e9817b06b19357f2a0
Saurav Kumar <maxsaurav267@gmail.com>
2021-04-03 14:03:04 +0530

    exported components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { Navbar } from "./Navbar";
 export { FilterBox } from "./FilterBox";
 export { ProductCard } from "./ProductCard";
+export { ProductPage } from "./ProductPage"
+export { Cart } from "./Cart";
+export { WishList } from "./Wishlist";
