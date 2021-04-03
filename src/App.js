421b1c66f5e93c57c3375ca886fdd2079fbcedbe
Saurav Kumar <maxsaurav267@gmail.com>
2021-04-03 13:59:08 +0530

    added routes for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { Navbar, FilterBox, ProductCard } from "./components";
+import { Navbar, FilterBox, ProductCard , ProductPage, Cart, WishList} from "./components";
+import { useProductDetails } from "./context/productpage-context";
+import "./App.css";
+
 
 export default function App() {
-  
+  const { routes, cartState } = useProductDetails();
+
+  function Products(){
+    return(
+      <div className="container">
+        { cartState.items !== null && <ProductPage/> }
+        <FilterBox/>
+        <ProductCard/> 
+      </div>
+    )
+  }
   return (
     <div className="App">
       <Navbar/>
-      <FilterBox/>
-      <ProductCard/>
+      { routes === "products"  && <Products/> }
+      { routes === "cart" && <Cart/> }
+      { routes === "wishlist" && <WishList/> }
     </div>
   );
 }
