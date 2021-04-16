f8f081f21bd0c4addbf0ae2ed051a453b6eab03a
Saurav Kumar <maxsaurav267@gmail.com>
2021-04-16 14:51:14 +0530

    added react router routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import { Navbar, FilterBox, ProductCard , ProductPage, Cart, WishList} from "./components";
+import { Home, ProductNavbar, FilterBox, ProductCard , ProductPage, Cart, WishList, ErrorPage} from "./components";
 import { useProductDetails } from "./context/productpage-context";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 
 export default function App() {
-  const { routes, cartState } = useProductDetails();
+  const { cartState } = useProductDetails();
 
   function Products(){
     return(
-      <div className="container">
-        { cartState.items !== null && <ProductPage/> }
-        <FilterBox/>
-        <ProductCard/> 
-      </div>
+      <>
+        <ProductNavbar/>
+        <div className="container">
+          { cartState.items !== null && <ProductPage/> }
+          <h1 style={{margin: "6rem 0 0 0", textAlign: "center", color: "rgb(68, 69, 74)"}}>Products</h1>
+          <FilterBox/>
+          <ProductCard/> 
+        </div>
+      </>
     )
   }
   return (
     <div className="App">
-      <Navbar/>
-      { routes === "products"  && <Products/> }
-      { routes === "cart" && <Cart/> }
-      { routes === "wishlist" && <WishList/> }
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/products" element={ <Products/> } />
+        <Route path="/cart" element={ <Cart/>} />
+        <Route path="/wishlist" element={ <WishList/>} />
+        <Route path="*" element={ <ErrorPage/> } />
+      </Routes>
+      
     </div>
   );
 }
