e8e88fade645ac2c8ed1d8fe8f954d3e52572593
Saurav Kumar <maxsaurav267@gmail.com>
2021-04-16 10:31:20 +0530

    refactor filter component

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -4,71 +4,71 @@ export function FilterBox(){
     const { state, dispatch } = useProduct();
     const { showInventoryAll, showFastDeliveryOnly, sortBy, priceRange, toggleFilter } = state;
 
-    function Filters(){
-        return(
-            <div>
-                <div className="sorting">
-                    <p>Price</p>
-                    <label>
-                        <input 
-                        type="radio"
-                        name="price-sort"
-                        checked={sortBy === "LOW_TO_HIGH"}
-                        onChange={() => {
-                            dispatch({type: "SORT", payload: "LOW_TO_HIGH"})
-                        }}
-                        />
-                        Low to High
-                    </label>
+    // function Filters(){
+    //     return(
+    //         <div>
+    //             <div className="sorting">
+    //                 <p>Price</p>
+    //                 <label>
+    //                     <input 
+    //                     type="radio"
+    //                     name="price-sort"
+    //                     checked={sortBy === "LOW_TO_HIGH"}
+    //                     onChange={() => {
+    //                         dispatch({type: "SORT", payload: "LOW_TO_HIGH"})
+    //                     }}
+    //                     />
+    //                     Low to High
+    //                 </label>
 
-                    <label>
-                        <input 
-                        type="radio"
-                        name="price-sort"
-                        checked={sortBy === "HIGH_TO_LOW"}
-                        onChange={() => {
-                            dispatch({type: "SORT", payload: "HIGH_TO_LOW"})
-                        }}/>
-                        High to Low
-                    </label>
+    //                 <label>
+    //                     <input 
+    //                     type="radio"
+    //                     name="price-sort"
+    //                     checked={sortBy === "HIGH_TO_LOW"}
+    //                     onChange={() => {
+    //                         dispatch({type: "SORT", payload: "HIGH_TO_LOW"})
+    //                     }}/>
+    //                     High to Low
+    //                 </label>
 
-                </div>
+    //             </div>
 
-                <div className="avability">
-                    <p>Avability</p>
-                    <label>
-                        <input 
-                        type="checkbox"
-                        checked={ showInventoryAll }
-                        onChange={() => dispatch({ type: "TOGGLE_INVENTORY" })}/>
-                        Include Out of Stock
-                    </label>
+    //             <div className="avability">
+    //                 <p>Avability</p>
+    //                 <label>
+    //                     <input 
+    //                     type="checkbox"
+    //                     checked={ showInventoryAll }
+    //                     onChange={() => dispatch({ type: "TOGGLE_INVENTORY" })}/>
+    //                     Include Out of Stock
+    //                 </label>
 
-                    <label>
-                        <input 
-                        type="checkbox"
-                        checked={ showFastDeliveryOnly }
-                        onChange={() => dispatch({ type: "TOGGLE_DELIVERY" })}/>
-                        Fast Delivery Only
-                    </label>
+    //                 <label>
+    //                     <input 
+    //                     type="checkbox"
+    //                     checked={ showFastDeliveryOnly }
+    //                     onChange={() => dispatch({ type: "TOGGLE_DELIVERY" })}/>
+    //                     Fast Delivery Only
+    //                 </label>
 
-                </div>
+    //             </div>
 
-                <div className="price-range">
-                    <p>Price Range</p> 
-                    <label>
-                        Below Rs. {priceRange}
-                        <input 
-                            onChange={(e) => dispatch({type: "PRICE_RANGE", payload: e.target.value})} 
-                            type="range" 
-                            max="1000" 
-                        />
-                    </label>
-                </div>
+    //             <div className="price-range">
+    //                 <p>Price Range</p> 
+    //                 <label>
+    //                     Below Rs. {priceRange}
+    //                     <input 
+    //                         onChange={(e) => dispatch({type: "PRICE_RANGE", payload: e.target.value})} 
+    //                         type="range" 
+    //                         max="1000" 
+    //                     />
+    //                 </label>
+    //             </div>
 
-            </div>
-        )
-    }
+    //         </div>
+    //     )
+    // }
 
     return (
         <div className="filter-box">
