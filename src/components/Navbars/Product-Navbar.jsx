bb903b5a0e9bf32f6e2c2f3a9cf178d4b956089f
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-06 23:45:09 +0530

    fix: cart and wishlist length

--- src/components/Navbars/Product-Navbar.jsx
+++ src/components/Navbars/Product-Navbar.jsx
@@ -1,43 +1,52 @@
 import { useProduct } from "../../context/product-context"
-import { useProductDetails } from "../../context/productpage-context";
+import { useCart } from "../../context/cart-context";
 import { Link } from "react-router-dom";
+import { useWishlist } from "../../context/wishlist-context";
 
 
 export function ProductNavbar(){
-    const {dispatch} = useProduct();
-    const { cartState } = useProductDetails();
+    const {state, dispatch} = useProduct();
+    const { cartState } = useCart();
+    const { wishlistState } = useWishlist();
+    const { wishlist } = wishlistState;
+    const { cart } = cartState;
+    
     return(
         <div>
             <nav className="navbar">
-            <Link to="/" className="home-link">MyMart</Link>
+                <Link to="/" className="home-link"> MyMart </Link>
 
-            <div className="hamburger">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+                <div className="hamburger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
 
-            <ul className="navbar-lists">
-                <li>
-                    <input onChange={(e)=> dispatch({type: "SEARCH", payload: e.target.value})} className="search-box" type="text" placeholder="Search"/>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/cart" className="nav-link">
-                        Cart 
-                        { cartState.itemsInCart.length > 0 && <span className="items-count">{ cartState.itemsInCart.length }</span> } 
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/wishlist" className="nav-link">
-                        WishList 
-                        { cartState.itemsInWishList.length > 0 && <span className="items-count">{ cartState.itemsInWishList.length }</span> }
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+                <ul className="navbar-lists">
+                    <li>
+                        <input onChange={(e)=> dispatch({type: "SEARCH", payload: e.target.value})} 
+                            value={ state.searchInputValue }
+                            className="search-box" 
+                            type="text" 
+                            placeholder="Search"/>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart" className="nav-link">
+                            Cart 
+                            { cart.length > 0 && <span className="items-count">{ cart.length }</span> } 
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/wishlist" className="nav-link">
+                            WishList 
+                            { wishlist.length > 0 && <span className="items-count">{ wishlist.length }</span> }
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
