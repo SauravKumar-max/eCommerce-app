61aa4132c2efeeb68176f1e1fe040925a6e0f40b
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-06 23:40:47 +0530

    refactor: cart reducer function

---
+++ src/reducer/cart-reducer.js
@@ -0,0 +1,24 @@
+
+export function cartReducer(cartState, action ){
+      
+    switch (action.type) {
+        case "CART_DATA":
+                return { ...cartState, cart: action.payload };
+
+        case "ADD_TO_CART":
+                return {...cartState, cart: [...cartState.cart, action.payload]};
+        
+        case "DELETE_FROM_CART":
+            return {...cartState, cart: cartState.cart.filter(item => item._id !== action.payload)};
+        
+        case "INCREASE_QUANTITY":
+            return {...cartState, cart: cartState.cart.map(item => item._id === action.payload ? {...item, quantity: item.quantity + 1} : item)};
+            
+        case "DECREASE_QUANTITY": 
+            return {...cartState, cart: cartState.cart.map(item => item._id === action.payload ? {...item, quantity: item.quantity - 1} : item)};
+
+        default:
+            break;
+    }
+
+} 
