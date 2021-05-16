a76e9815aec31e5d07591719db4cb5e46c4782b6
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-16 19:49:33 +0530

    added loader useState

--- src/context/product-context.jsx
+++ src/context/product-context.jsx
@@ -11,23 +11,41 @@ export function useProduct() {
 export function ProductProvider({children}){
     const api = "https://ecommerce-backend.sauravkumar007.repl.co/products";
     const [ data, setData ] = useState([]);
+    const [ loader, setLoader ] = useState(true);
 
     useEffect(() => {
         (async function () {
             try {
                 const response = await axios.get(api);
                 setData(response.data.products);
+                setLoader(false);
             } catch (error) {
                 console.log(error);
             }
         })();
       }, []);
+
+      const stateObject = { 
+          showInventoryAll: true, 
+          showFastDeliveryOnly: false, 
+          sortBy: null, 
+          searchInputValue: "", 
+          priceRange: "1000", 
+          toggleFilter: false, 
+          item: null, 
+          toast: {
+                toShow: false,
+                message: "toast message will be here." 
+            },
+           screenLoader: false, 
+            
+        }
     
 
-    const [state, dispatch] = useReducer(productReducer,  { showInventoryAll: true, showFastDeliveryOnly: false, sortBy: null, searchInputValue: "", priceRange: "1000", toggleFilter: false, item: null});
+    const [state, dispatch] = useReducer(productReducer, stateObject );
 
     return (
-        <ProductList.Provider value={{ api, data, state, dispatch}}>
+        <ProductList.Provider value={{ loader, api, data, state, dispatch}}>
             {children}
         </ProductList.Provider>
     )
