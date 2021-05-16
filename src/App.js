9acdd1c9a8e31ac5c5ffea1257580277851dc8a2
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-16 19:40:49 +0530

    added toast and loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Home, Navbar, Cart, WishList, ErrorPage, Products} from "./components/index";
+import { Home, Navbar, Cart, WishList, ErrorPage, Products, Toast} from "./components/index";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useProduct } from "./context/product-context";
 
 export default function App() {
+const { state } = useProduct();
   return (
     <div className="App">
       <Navbar/>
@@ -13,7 +15,8 @@ export default function App() {
         <Route path="/wishlist" element={ <WishList/>} />
         <Route path="*" element={ <ErrorPage/> } />
       </Routes>
-      
+      { state.toast.toShow && <Toast/> }
+      {state.screenLoader && <div className="full-loader"></div>}
     </div>
   );
 }
