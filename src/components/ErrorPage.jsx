5d991da78c1ca45de036665bc0182f6003f80055
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-07 22:29:07 +0530

    component moved

--- src/components/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-export function ErrorPage(){
-    return (
-        <div style={{fontSize: "3rem", fontWeight: "bold", textAlign: "center", margin: "6rem", color: "red" }}>
-            <p style={{margin: "2rem"}}>404</p>
-            <p>Page Not Found</p>
-            <i style={{fontSize: "7rem", margin: "2rem"}} className="fas fa-exclamation-triangle"></i>
-        </div>
-    )
-}
