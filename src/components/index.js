d5b5a57d5eb5be896aa89fb18d7c139d812de2b9
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-11 19:26:17 +0530

    exported components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
 export { Navbar } from "./Navbar";
 export { FilterBox } from "./FilterBox";
 export { ProductCard } from "./ProductCard";
-export { ErrorPage } from  "./ErrorPage";
 export { AddToCartBtn } from "./AddToCartBtn";
+export { AddToWishlistBtn } from "./AddToWishlistBtn";
 export { Toast } from "./Toast";
+export { PrivateRoute } from './PrivateRoute';
+export { ProductPage } from "./ProductPage";
+export { Loader } from "./Loader";
+export { ConfirmRemoveModal } from "./ConfirmRemoveModal";
+export { ProfileCard } from "./ProfileCard";
