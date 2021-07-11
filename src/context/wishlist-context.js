1b1f7523ed0a551e214c1d92bb7cc2819afe239f
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-11 19:50:25 +0530

    added token as guard and dependency

--- src/context/wishlist-context.js
+++ src/context/wishlist-context.js
@@ -1,25 +1,30 @@
-import axios from "axios";
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { wishlistReducer } from "../reducer/wishlist-reducer";
-
+import { useAuth } from "./auth-context";
+import axios from "axios";
+ 
 const Wishlist = createContext();
 
 export function WishlistProvider({children}){
-
     const [ wishlistState, dispatchWishlist ] = useReducer(wishlistReducer, { wishlist: []});
+    const { token } = useAuth();
 
     const api = "https://ecommerce-backend.sauravkumar007.repl.co/wishlists";
     useEffect(() => {
         (async () => {
-            try {
-                const response = await axios.get(api);
-                const fetchWishlist = response.data.wishlist;
-                dispatchWishlist({ type: "WISHLIST_DATA", payload: fetchWishlist });
-            } catch (error) {
-                console.error(error);
+            if(token){
+                try {
+                    const response = await axios.get(api);
+                    const fetchWishlist = response.data.userWishlist;
+                    dispatchWishlist({ type: "WISHLIST_DATA", payload: fetchWishlist });
+                } catch (error) {
+                    if(error.response.status === 401){
+                        console.error("some-thing is wrong!", error);
+                   }
+                 }
             }
         })()
-    }, [])
+    }, [token]);
 
     return(
         <Wishlist.Provider value={{ wishlistState, dispatchWishlist }}>
