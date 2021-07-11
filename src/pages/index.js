f1a90e3f27a3454317bdf4c084be3c68d95bbe78
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-11 19:24:33 +0530

    added login and signup page to export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-export { ProductPage } from "./ProductPage"
 export { Cart } from "./private/Cart";
 export { WishList } from "./private/Wishlist";
 export { Home } from "./Home";
 export { Products } from "../pages/AllProductsPage";
+export { Login } from "./Login"
+export { ErrorPage } from  "./ErrorPage";
+export { SingUp } from "./Signup";
