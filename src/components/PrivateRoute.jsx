1b0a08622d76921f1ce85e3af4f1d8a722d0d7c0
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-11 19:20:18 +0530

    created private route component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,9 @@
+import { Route, Navigate } from "react-router-dom";
+import { useAuth } from "../context/auth-context";
 
-// export function PrivateRoute(){
-//     return <Route {...props} element={login ? element : <Login/> } />
-// }
+export function PrivateRoute({path, ...props}){
+    const { login } = useAuth();
+    return (
+        login ? <Route {...props} path={path} /> : <Navigate state={{ from: path }} replace to="/login" />
+    )
+}
