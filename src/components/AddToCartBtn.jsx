38c04078f8fa346786d11f206a8bd860e5ae5ee7
Saurav Kumar <maxsaurav267@gmail.com>
2021-07-11 23:38:04 +0530

    added loader and redirect to login if not login

--- src/components/AddToCartBtn.jsx
+++ src/components/AddToCartBtn.jsx
@@ -1,30 +1,49 @@
 import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/auth-context";
 import { useCart } from "../context/cart-context";
 import { useProduct } from "../context/product-context";
+import { Loader } from "./Loader";
 
 export function AddToCartBtn({item}){
+    const [ spinner, setSpinner ] = useState(false);
     const { dispatch } = useProduct()
     const { dispatchCart } = useCart();
+    const { login } = useAuth();
+    const navigate = useNavigate();
 
-     const api = "https://ecommerce-backend.sauravkumar007.repl.co/carts";
-     const addtoCart = async (product) => {
-        dispatch({type: "LOAD_LOADER"});
+
+    const addtoCart = async (product) => {
+        setSpinner(true);
         try {
-            await axios.post(api, { _id: product._id, quantity: 1 });
-            dispatchCart({ type: "ADD_TO_CART", payload: product});
-            dispatch({type: "SHOW_TOAST", payload:`${product.name} Added to Cart.`});
-            dispatch({type: "HIDE_LOADER"});
+            const api = "https://ecommerce-backend.sauravkumar007.repl.co/carts";
+            const response = await axios.post(api, { _id: product._id, quantity: 1 });
+            if(response.status === 200){
+                setSpinner(false);
+                dispatchCart({ type: "ADD_TO_CART", payload: product});
+                dispatch({type: "SHOW_TOAST", payload:`${product.name} Added to Cart.`});
+            }
         } catch (error) {
             console.error(error);
         }
-     }
+    }
     
     return(
-        <button className={ item.inStock ? "text-icon-btn" : "text-icon-btn out-of-stock"}
-            disabled={!item.inStock} 
-            onClick={ async () =>  addtoCart(item)}>
-            <i className="fas fa-shopping-cart"></i>
-            {item.inStock ? "Add To Cart" : "Out of Stock"}
-        </button>
+        <>
+        {
+            spinner ? <button className="primary-btn"> <Loader color={'#fff'}/> </button>
+            
+            : 
+        
+            <button className={ item.inStock ? "text-icon-btn" : "text-icon-btn out-of-stock"}
+                disabled={!item.inStock} 
+                onClick={ login ? async () =>  addtoCart(item) : () => navigate('/login')}
+            >
+                <i className="fas fa-shopping-cart"></i>
+                {item.inStock ? "Add To Cart" : "Out of Stock"}
+            </button>
+        }   
+        </>
     )
 }
