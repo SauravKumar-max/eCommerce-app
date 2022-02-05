e54239b7b1f51cf6a61650e143275f29fabd8a53
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-05 21:49:29 +0530

    exported from all contexts

---
+++ src/context/index.ts
@@ -0,0 +1,8 @@
+export { ProductProvider } from "./product-context";
+export { CartProvider } from "./cart-context";
+export { AuthProvider } from "./auth-context";
+export { WishlistProvider } from "./wishlist-context";
+export { useProduct } from "./product-context";
+export { useCart } from "./cart-context";
+export { useWishlist } from "./wishlist-context";
+export { useAuth } from "./auth-context";
