1870a4c1ebb574751760e5ebca17853047fed3bb
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-05 22:02:26 +0530

    utils types

---
+++ src/utils/utils.types.ts
@@ -0,0 +1,36 @@
+import { Product } from "../context/context.types";
+
+export type Wishlist = {
+    userWishlist: Product[];
+}
+
+export type Cart = {
+    userCart: Product[]
+}
+
+export type User = {
+    email: string;
+    name: string;
+    address: Address[]
+}
+
+export type Address = {
+    _id: string;
+    name: string;
+    phone: string;
+    pincode: string;
+    street: string;
+    locality: string;
+    city: string;
+    state: string;
+}
+
+export type CarouselContent = {
+    image: string;
+}
+
+export type ExtraContent = {
+    image: string;
+    text: string;
+    offer: string;
+}
