9fffb2577acb1d09c86a450162e79d71fde9d19f
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-05 21:25:53 +0530

    feat: added providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router } from "react-router-dom";
+import { theme } from "./theme";
+import {
+  ProductProvider,
+  AuthProvider,
+  CartProvider,
+  WishlistProvider,
+} from "./context/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <ProductProvider>
+                <App />
+              </ProductProvider>
+            </WishlistProvider>
+          </CartProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
