36d1e88ada99cb1e9cb64040856fe0a94fbd98a3
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-05 22:02:10 +0530

    requesting user wishlist data

---
+++ src/utils/wishlistService.ts
@@ -0,0 +1,20 @@
+import { ServerError } from "../context/context.types";
+import axios, {AxiosError} from "axios";
+import { Wishlist } from "./utils.types";
+
+export async function getWishlist(): Promise<Wishlist | ServerError>{
+    try{
+        const api = process.env.REACT_APP_API_URL + "/wishlists";
+        const response = await axios.get<Wishlist>(api);
+        return response.data;
+    }catch(error){
+        if (axios.isAxiosError(error)) {
+            const serverError = error as AxiosError<ServerError>;
+            if (serverError && serverError.response) {
+              return serverError.response.data;
+            }
+        }
+        console.log(error)
+        return { message: "something snapped!" };
+    }
+}
