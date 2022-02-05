5f11a62ed145b85577e791bb86cbd1d4bda4d221
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-05 22:09:58 +0530

    final commit

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export { Home } from "./Home";
+export { Products } from "./Products";
+export { ProductDetail } from "./ProductDetail";
+export { Cart } from "./private/Cart";
+export { Wishlist } from "./private/Wishlist";
+export { Login } from "./Login";
+export { Signup } from "./Signup";
+export { PaymentStatus } from "./private/PaymentStatus";
+export { Address } from "./private/Address";
