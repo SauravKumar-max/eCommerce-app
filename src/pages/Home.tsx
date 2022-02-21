a547f80f903d6870a90681c5e2e85a3faa10445a
Saurav Kumar <maxsaurav237@gmail.com>
2022-02-21 15:01:33 +0530

    added offer text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography, Button } from "@mui/material";
 import { Carousel } from "../components";
+import { useNavigate } from "react-router-dom";
 
 export function Home(): JSX.Element {
+  const navigate = useNavigate();
   return (
     <Box sx={{ marginTop: { xs: "4rem", sm: "5rem" } }}>
       <Box sx={{ m: { xs: 1, sm: 2 } }}>
         <Carousel />
       </Box>
+      <Stack alignItems={"center"} sx={{ paddingBottom: "2rem" }}>
+        <Typography
+          sx={{
+            fontSize: { xs: "2rem", md: "3rem" },
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "text.primary",
+          }}
+        >
+          Shop On Top Brands
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: { xs: "2rem", md: "3rem" },
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "text.primary",
+          }}
+        >
+          Upto 60% Off
+        </Typography>
+        <Button
+          variant="outlined"
+          sx={{ fontWeight: "bold", border: "solid 1px #3d41d7" }}
+          onClick={() => navigate("/products")}
+        >
+          Shop Now
+        </Button>
+      </Stack>
     </Box>
   );
 }
