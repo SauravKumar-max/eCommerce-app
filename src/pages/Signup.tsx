ce3b9c7a187afda17f717f10a02632bdae2ee2d3
Saurav Kumar <maxsaurav237@gmail.com>
2023-04-21 13:34:48 +0530

    updated api url

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -70,7 +70,7 @@ export function Signup(): JSX.Element {
   async function signupHandler() {
     setSpinner(true);
     try {
-      const api = "https://ecommerce-backend.sauravkumar007.repl.co/user";
+      const api = process.env.REACT_APP_API_URL + "/user";
       const response = await axios.post(api, {
         name: inputValue.name.toUpperCase(),
         username: inputValue.username,
